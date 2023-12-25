ff5837d3ea282270a048d6e5fa930f93fd5f79b0
Abuthahir98 <127249703+Abuthahir98@users.noreply.github.com>
2023-12-25 13:12:54 -0500

    Update and rename Main.py to app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from flask_ngrok import run_with_ngrok
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when the app is run
+
+# Load the pre-trained model
+model = joblib.load('stock_prediction_model.joblib')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # Fetch real-time data every 1 minute
+    ticker = 'AAPL'
+    data = yf.download(ticker, period='1d', interval='1m').tail(5)
+
+    # Simulate trading
+    future_data = pd.DataFrame(columns=['predicted'])
+    for _ in range(5):
+        next_time_point = data.tail(1)
+        next_time_point.drop(columns=['Returns'], inplace=True)
+
+        # Predict the next closing price
+        predicted_price = model.predict(next_time_point[features])
+        future_data = future_data.append({'predicted': predicted_price[0]}, ignore_index=True)
+
+    return jsonify({'predictions': future_data.to_dict()})
+
+if __name__ == '__main__':
+    app.run()
